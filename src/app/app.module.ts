fdeb4cb05ec5752ccb4a088257c657ac48c2056f
John Wilfred Montilde <wilfredjohnmontilde19@gmail.com>
2022-05-21 18:18:11 +0800

    Update app module with the new created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
+import { RxStompService } from '../common/services/rx-stomp.service'
+import { rxStompServiceFactory } from '../common/services/rx-stomp-service-factory';
+import { MessagesComponent } from './messages/messages.component';
+import { StatusComponent } from './status/status.component';
+import { SendMessageComponent } from './send-message/send-message.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent,
+    StatusComponent,
+    SendMessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: RxStompService,
+      useFactory: rxStompServiceFactory,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/common/models/message.interface.ts
@@ -0,0 +1,4 @@
+export interface IMessage {
+  date: Date
+  body: string
+}
